e30fab3e2243edcead7c02e602d723c106caecc4
RemagOfficial <lloydfb456@gmail.com>
2026-05-16 11:31:10 +0100

    port to 1.21.1

--- common/src/main/java/de/keksuccino/panoramica/mixin/mixins/common/client/MixinMinecraft.java
+++ common/src/main/java/de/keksuccino/panoramica/mixin/mixins/common/client/MixinMinecraft.java
@@ -2,11 +2,9 @@ package de.keksuccino.panoramica.mixin.mixins.common.client;
 
 import de.keksuccino.panoramica.PanoramicaHandler;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.Timer;
 import net.minecraft.client.server.IntegratedServer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -19,8 +17,6 @@ import javax.annotation.Nullable;
 public abstract class MixinMinecraft {
 
     @Shadow private volatile boolean pause;
-    @Shadow private float pausePartialTick;
-    @Shadow @Final private Timer timer;
     @Shadow @Nullable private IntegratedServer singleplayerServer;
 
     @Unique private static final Logger LOGGER_PANORAMICA = LogManager.getLogger();
@@ -35,12 +31,11 @@ public abstract class MixinMinecraft {
     }
 
     @Inject(method = "runTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/DebugScreenOverlay;logFrameDuration(J)V"))
-    private void afterSetPause_Panoramica(boolean $$0, CallbackInfo info) {
+    private void afterSetPause_Panoramica(CallbackInfo info) {
         //Force-pause game when taking panorama screenshot (will automatically get reset by runTick() when panoramicMode is false again)
         boolean isPausePossible = this.hasSingleplayerServer() && (this.singleplayerServer != null) && !this.singleplayerServer.isPublished();
         if (isPausePossible && PanoramicaHandler.panoramaMode && !this.pause) {
             this.pause = true;
-            this.pausePartialTick = this.timer.partialTick;
         }
     }
 
--- forge/src/main/java/de/keksuccino/panoramica/platform/ForgeCompatibilityLayer.java
+++
@@ -1,7 +0,0 @@
-package de.keksuccino.panoramica.platform;
-
-import de.keksuccino.panoramica.platform.services.IPlatformCompatibilityLayer;
-
-public class ForgeCompatibilityLayer implements IPlatformCompatibilityLayer {
-
-}
